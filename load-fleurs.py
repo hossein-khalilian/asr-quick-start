21f345a72a0a6521c2695638769c84bcb65eb26e
hossein-khalilian <hse.khalilian08@gmail.com>
2025-06-30 09:36:18 +0330

    update load dataset script

--- load-fleurs.py
+++
@@ -1,4 +0,0 @@
-from datasets import load_dataset
-
-lang_code = "fa_ir"
-fleurs = load_dataset("google/fleurs", lang_code, split="test", trust_remote_code=True)
---
+++ load_dataset.py
@@ -0,0 +1,25 @@
+from datasets import load_dataset
+
+# dataset = load_dataset("SeyedAli/Persian-Speech-Dataset", trust_remote_code=True)
+
+# dataset = load_dataset("Kamtera/ParsiGoo", trust_remote_code=True)
+
+# dataset = load_dataset("PerSets/filimo-persian-asr", trust_remote_code=True)
+
+# dataset = load_dataset("PerSets/youtube-persian-asr", trust_remote_code=True)
+
+# dataset = load_dataset(
+#     "pariajm/sharif_emotional_speech_dataset", trust_remote_code=True
+# )
+
+# dataset = load_dataset(
+#     "MohammadGholizadeh/costumer-service-persian-1", trust_remote_code=True
+# )
+
+# dataset = load_dataset("espnet/floras", "multilingual")
+
+
+dataset = load_dataset("wuenlp/fleurs-belebele", "pes_Arab", trust_remote_code=True)
+
+
+print(dataset)
